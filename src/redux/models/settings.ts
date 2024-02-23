4a32788fa9d5343d15a38f7a911672c3a1dc7e7e
drafish <xwlyy1991@163.com>
2024-02-23 19:20:25 +0800

    add custom network

--- src/redux/models/settings.ts
+++ src/redux/models/settings.ts
@@ -1,6 +1,48 @@
 import { type ModelType } from '../store';
 import injected from '../../utils/injected';
 
+const addCustomNetwork = async (payload: {
+  chainName?: string;
+  chainId: string;
+  rpcUrls?: Array<string>;
+  nativeCurrency?: Record<string, any>;
+  blockExplorerUrls?: Array<string>;
+}) => {
+  const { chainId, chainName, rpcUrls, nativeCurrency, blockExplorerUrls } =
+    payload;
+  try {
+    await (window as any).ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: chainId }],
+    });
+  } catch (switchError: any) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (switchError.code === 4902) {
+      try {
+        const paramsObj: Record<string, any> = {
+          chainId: chainId,
+          chainName: chainName,
+          rpcUrls: rpcUrls,
+        };
+        if (nativeCurrency) paramsObj.nativeCurrency = nativeCurrency;
+        if (blockExplorerUrls) paramsObj.blockExplorerUrls = blockExplorerUrls;
+        await (window as any).ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: [paramsObj],
+        });
+
+        await (window as any).ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: chainId }],
+        });
+      } catch (addError) {
+        // handle "add" error
+      }
+    }
+    // handle other "switch" errors
+  }
+};
+
 const Model: ModelType = {
   namespace: 'settings',
   state: {
@@ -24,13 +66,31 @@ const Model: ModelType = {
     *connect({ payload }, { put }) {
       const { networkName } = payload;
       yield put({ type: 'settings/save', payload: { networkName } });
-      const chainId = networkName.match(/\(([^)]+)\)/)[1];
+      const chainId =
+        '0x' + Number(networkName.match(/\(([^)]+)\)/)[1]).toString(16);
+      const paramsObj: any = { chainId };
+      if (chainId === '0xa') {
+        paramsObj.chainName = 'Optimism';
+        paramsObj.rpcUrls = ['https://mainnet.optimism.io'];
+      }
+      if (chainId === '0xa4b1') {
+        paramsObj.chainName = 'Arbitrum One';
+        paramsObj.rpcUrls = ['https://arb1.arbitrum.io/rpc'];
+      }
+      if (chainId === '0x50877ed6') {
+        paramsObj.chainName = 'SKALE Chaos Testnet';
+        paramsObj.rpcUrls = [
+          'https://staging-v3.skalenodes.com/v1/staging-fast-active-bellatrix',
+        ];
+        paramsObj.nativeCurrency = {
+          name: 'sFUEL',
+          symbol: 'sFUEL',
+          decimals: 18,
+        };
+      }
       // @ts-expect-error
       const web3Provider = window.ethereum;
-      yield web3Provider.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x' + Number(chainId).toString(16) }],
-      });
+      yield addCustomNetwork(paramsObj);
       yield web3Provider.request({ method: 'eth_requestAccounts' });
       injected.setProvider(web3Provider);
       injected.getAccounts();
