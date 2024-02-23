f56bdac3d20fb707565686ce46737349b281a457
drafish <xwlyy1991@163.com>
2024-02-23 15:51:15 +0800

    update

--- src/components/SettingsUI/network.tsx
+++ src/components/SettingsUI/network.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useAppSelector } from '../../redux/hooks';
 
 export function NetworkUI() {
-  const networkName = useAppSelector((state) => state.settings.networkName);
+  const networkName = useAppSelector((state) => state.instance.network);
   return (
     <div className="">
       <div className="udapp_settingsLabel"></div>
