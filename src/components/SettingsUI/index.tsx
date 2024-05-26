2ad5554f3f1b072332fa5faee3e9df3795655fb6
drafish <xwlyy1991@163.com>
2024-05-26 09:09:38 +0800

    add viewSourceCode button

--- src/components/SettingsUI/index.tsx
+++ src/components/SettingsUI/index.tsx
@@ -13,46 +13,59 @@ import { AppContext } from '../../contexts';
 export function SettingsUI() {
   const intl = useIntl();
   const { appState } = useContext(AppContext);
-  const { balance, name, address } = appState.instance;
+  const { balance, name, address, verified } = appState.instance;
 
   return (
     <div className="px-4">
-      <div className="mb-3 p-2 w-auto d-flex justify-content-between align-items-center">
-        <div>Remix DApp</div>
+      <div className="p-2 w-auto d-flex justify-content-between align-items-center">
+        <h1>Remix DApp</h1>
         <LocaleUI />
       </div>
-      <NetworkUI />
-      <div className="bg-transparent d-flex m-0 p-0 border-0 alert alert-secondary">
-        <div
-          className="input-grou w-100 flex-nowrap"
-          style={{ display: 'contents' }}
-        >
-          <div className="w-100 input-group-prepend">
-            <span
-              className="input-group-text border-0 p-0 bg-transparent text-uppercase"
-              style={{ fontSize: 11 }}
-            >
-              {name} at {shortenAddress(address)}
-            </span>
-          </div>
-          <div className="btn">
-            <CopyToClipboard
-              tip={intl.formatMessage({ id: 'udapp.copy' })}
-              content={address}
-              direction={'top'}
-            />
+      <div className="bg-light my-4 p-3">
+        <NetworkUI />
+        <div className="bg-transparent d-flex m-0 p-0 border-0 alert alert-secondary">
+          <div
+            className="input-grou w-100 flex-nowrap"
+            style={{ display: 'contents' }}
+          >
+            <div className="w-100 input-group-prepend">
+              <span
+                className="input-group-text border-0 p-0 bg-transparent text-uppercase"
+                style={{ fontSize: 11 }}
+              >
+                {name} at {shortenAddress(address)}
+              </span>
+            </div>
+            <div className="btn">
+              <CopyToClipboard
+                tip={intl.formatMessage({ id: 'udapp.copy' })}
+                content={address}
+                direction={'top'}
+              />
+            </div>
           </div>
         </div>
+        <div className="d-flex" data-id="instanceContractBal">
+          <label>
+            <FormattedMessage id="udapp.balance" />: {balance} ETH
+          </label>
+        </div>
+        <AccountUI />
+        <GasPriceUI />
+        <ValueUI />
+        {/* <ThemeUI /> */}
       </div>
-      <div className="d-flex" data-id="instanceContractBal">
-        <label>
-          <FormattedMessage id="udapp.balance" />: {balance} ETH
-        </label>
-      </div>
-      <AccountUI />
-      <GasPriceUI />
-      <ValueUI />
-      {/* <ThemeUI /> */}
+      {verified && (
+        <a
+          href={`https://remix.ethereum.org/address/${address}`}
+          className="btn btn-primary mt-4"
+          role="button"
+          aria-disabled="true"
+          target="_blank"
+        >
+          <FormattedMessage id="udapp.viewSourceCode" />
+        </a>
+      )}
     </div>
   );
 }
